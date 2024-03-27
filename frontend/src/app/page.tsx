e0a6d0e608c036bcdcdcb4abe81550dc353c1ed1
RT <rustytrim11@gmail.com>
2024-03-26 21:27:07 -0400

    Update card for todos

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 import Card from "@/components/Card";
 import { useController } from "@/controller";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const { toggleSidebar, sidebarActive } = useController();
 
+  const [test, setTest] = useState<Date>();
+
+  useEffect(() => {
+    setTest(new Date());
+  }, []);
+
   return (
     <main>
       <h1 className="title hover:text-blue-400" onClick={() => toggleSidebar(!sidebarActive)}>Home</h1>
       <div className="w-full pl-4 pr-8 my-4">
-        <Card title="Example Todo" description="Example Todo Description" tag="Design" />
+        <Card title="Example Todo" description="Example Todo Description" tag="Design" _active={true} due={test} />
       </div>
     </main>
   );
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,16 +1,25 @@
+import Link from "next/link";
+import { useState } from "react";
 import { FaCheck, FaCheckCircle, FaTag, FaTrash } from "react-icons/fa";
 import { FaX } from "react-icons/fa6";
 
-export default function Card({ title, description, tag, className }: { title: string, description: string, tag: string, className?: string }) {
+export default function Card({ title, description, tag, due, className, _active }: { title: string, description: string, tag: string, due?: Date, className?: string, _active: boolean }) {
+
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const [active, setActive] = useState(_active);
+
     return (
-        <div className={"bg-card p-4 rounded-md border border-main" + (' ' + className ?? '')}>
+        <div style={{ opacity: active ? "100%" : "25%" }} className={"bg-card p-4 rounded-md border border-main relative" + (' ' + className ?? '')}>
+            {!active && <div className="absolute w-full top-1/4 left-0 h-0.5 bg-white"></div>}
             <span className="gap-8 flex items-center">
                 <h1 className="font-bold text-lg flex items-center gap-2"><FaCheckCircle /> {title}</h1>
-                <h2 className="font-semibold text-lg flex items-center gap-2"><FaTag /> {tag}</h2>
+                <Link href={`/tags/${tag.toLowerCase()}`} className="font-semibold text-lg flex items-center gap-2"><FaTag /> {tag}</Link>
             </span>
+            {due && <time className="font-serif">Due {months[due.getMonth()]} {due.getDate() - 1}, {due.getFullYear()}</time>}
             <p className="text-sm">{description}</p>
             <div className="pt-4 flex gap-4">
-                <button className="p-2 rounded-full bg-blue-500" aria-label="Complete Task"><FaCheck /></button>
+                <button onClick={() => setActive((prev) => !prev)} className={`p-2 rounded-full bg-${active ? "blue" : "red"}-500`} aria-label="Complete Task">{active ? <FaCheck /> : <FaX />}</button>
                 <button className="p-2 rounded-full bg-red-500"><FaTrash /></button>
             </div>
         </div>
